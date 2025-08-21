a1f042248e78ee80306ece2619bf7a518a0c5728
tcezard <tcezard@ebi.ac.uk>
2025-08-21 14:41:33 +0100

    Run eva-sub-cli using repository/branch/sha that triggered integration test

---
+++ components/eva_sub_cli/install_repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+# Check if the repository has been set from the environment variable
+if [[ -z "$GITHUB_REPOSITORY" ]] ; then GITHUB_REPOSITORY=EBIvariation/eva-sub-cli ; fi
+if [[ -z "$GITHUB_REF" ]] ; then GITHUB_REF=main ; fi
+if [[ -n "$GITHUB_SHA" ]] ; then GITHUB_REF=$GITHUB_SHA ; fi
+
+git clone https://github.com/${GITHUB_REPOSITORY}.git eva-sub-cli
+cd eva-sub-cli
+git checkout ${GITHUB_REF}
+
+python -m pip install .
+
+cd ..
