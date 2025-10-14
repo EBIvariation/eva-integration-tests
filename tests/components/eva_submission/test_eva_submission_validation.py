4979ecc4724f5be7b4e85ebdf76d49f72e52a720
April Shen <ashen@ebi.ac.uk>
2025-10-14 16:06:18 +0100

    add test for tasks in eva-submission

--- tests/components/eva_submission/test_eva_submission_validation.py
+++ tests/components/eva_submission/test_eva_submission_validation.py
@@ -27,7 +27,7 @@ class TestEvaSubmissionValidation(TestWithDockerCompose):
     @classmethod
     def setUpClass(cls):
         # TODO: Remove before merge
-        os.environ['SOURCE_GITHUB_REPOSITORY'] = 'apriltuesday/eva-sub-cli'
+        os.environ['SOURCE_GITHUB_REPOSITORY'] = 'apriltuesday/eva-submission'
         os.environ['SOURCE_GITHUB_REF'] = 'EVA-3950'
 
         super().setUpClass()
@@ -69,6 +69,33 @@ class TestEvaSubmissionValidation(TestWithDockerCompose):
 
         self.assert_directory_structure(os.path.join(self.test_run_dir, f'ELOAD_{self.eload_number}'))
 
+    def test_validation_with_tasks(self):
+        prepare_cmd = (
+            f"docker exec {self.container_name} prepare_submission.py --submitter username --ftp_box 1 "
+            f"--eload {self.eload_number}"
+        )
+
+        # Run preparation from command line
+        run_quiet_command("run eva_submission prepare_submission script", prepare_cmd)
+
+        validation_cmd = (
+            f"docker exec {self.container_name} sh -c 'validate_submission.py --eload {self.eload_number} "
+            f"--validation_tasks metadata_check structural_variant_check "
+            f"> {self.container_eload_dir}/ELOAD_{self.eload_number}/validation.out'"
+        )
+        # Run validation from command line
+        run_quiet_command("run eva_submission validate_submission script", validation_cmd)
+
+        # copy validation output from docker
+        copy_files_from_container(self.container_name,
+                                  os.path.join(self.container_eload_dir),
+                                  self.test_run_dir)
+        # assert results
+        self.assert_validation_pass_in_config(os.path.join(self.test_run_dir, f'ELOAD_{self.eload_number}',
+                                                           f'.ELOAD_{self.eload_number}_config.yml'),
+                                              tasks=['metadata_check', 'structural_variant_check'])
+        self.assert_directory_structure(os.path.join(self.test_run_dir, f'ELOAD_{self.eload_number}'))
+
     def create_submission_dir_and_copy_files_to_container(self):
         vcf_file = os.path.join(self.vcf_files_dir, 'vcf_file_ASM294v2.vcf')
         copy_files_to_container(self.container_name, self.container_submission_dir, vcf_file)
@@ -81,12 +108,15 @@ class TestEvaSubmissionValidation(TestWithDockerCompose):
         copy_files_to_container(self.container_name, self.container_reference_genome_dir,
                                 os.path.join(self.fasta_files_dir, 'GCA_000002945.2.fa'))
 
-    def assert_validation_pass_in_config(self, eload_config_yml):
+    def assert_validation_pass_in_config(self, eload_config_yml, tasks=[
+        'vcf_check', 'assembly_check', 'metadata_check', 'sample_check', 'structural_variant_check',
+        'naming_convention_check'
+    ]):
         # Check that the config file exists
         assert os.path.isfile(eload_config_yml)
         config = Configuration(eload_config_yml)
         # Check that each validation does pass
-        for check in ['eva_sub_cli']:
+        for check in tasks:
             print(check)
             print(config.query('validation'))
             print(config.query('validation', check))
