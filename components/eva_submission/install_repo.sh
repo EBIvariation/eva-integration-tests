df2f26975f6033f997da5488f8b457821360312a
nkumar2 <nkumar2@ebi.ac.uk>
2026-03-31 16:14:39 +0100

    update tests

--- components/eva_submission/install_repo.sh
+++ components/eva_submission/install_repo.sh
@@ -7,6 +7,10 @@ if [[ -z "$SOURCE_GITHUB_REPOSITORY" ]] ; then SOURCE_GITHUB_REPOSITORY=EBIvaria
 if [[ -z "$SOURCE_GITHUB_REF" ]] ; then SOURCE_GITHUB_REF=master ; fi
 if [[ -n "$SOURCE_GITHUB_SHA" ]] ; then SOURCE_GITHUB_REF=$SOURCE_GITHUB_SHA ; fi
 
+# TODO: Remove before merge
+SOURCE_GITHUB_REPOSITORY=nitin-ebi/eva-submission
+SOURCE_GITHUB_REF=store-submission-id-eload
+
 echo "Clone https://github.com/${SOURCE_GITHUB_REPOSITORY}.git"
 
 git clone https://github.com/${SOURCE_GITHUB_REPOSITORY}.git eva-submission
