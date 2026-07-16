d5ac6b128aa72e4d078e261350466f2ff41ab798
April Shen <ashen@ebi.ac.uk>
2026-07-16 16:36:17 +0100

    update submissions tables

--- components/postgres_db/03_eva_submissions.sql
+++ components/postgres_db/03_eva_submissions.sql
@@ -60,8 +60,6 @@ CREATE TABLE eva_submissions.submission_details (
 	metadata_json jsonb NOT NULL,
 	project_description varchar(5000) NOT NULL,
 	project_title varchar(500) NOT NULL,
-	project_accession varchar(255) NULL,
-	release_date timestamp NULL,
 	CONSTRAINT submission_details_pkey PRIMARY KEY (submission_id)
 );
 
@@ -69,16 +67,28 @@ ALTER TABLE eva_submissions.submission_details OWNER to metadata_db_user;
 GRANT ALL ON TABLE eva_submissions.submission_details to metadata_db_user;
 
 
---- table (submission_details_analysis_accessions)
-CREATE TABLE eva_submissions.submission_details_analysis_accessions (
+--- table (submission_tracking_details)
+CREATE TABLE eva_submissions.submission_tracking_details (
+	submission_id varchar(255) NOT NULL,
+	project_accession varchar(255) NULL,
+	release_date timestamp NULL,
+	CONSTRAINT submission_tracking_details_pkey PRIMARY KEY (submission_id)
+);
+
+ALTER TABLE eva_submissions.submission_tracking_details OWNER to metadata_db_user;
+GRANT ALL ON TABLE eva_submissions.submission_tracking_details to metadata_db_user;
+
+
+--- table (submission_tracking_details_analysis_accessions)
+CREATE TABLE eva_submissions.submission_tracking_details_analysis_accessions (
 	submission_id varchar(255) NOT NULL,
 	analysis_accessions varchar(255) NULL
 );
 
-ALTER TABLE eva_submissions.submission_details_analysis_accessions OWNER to metadata_db_user;
-GRANT ALL ON TABLE eva_submissions.submission_details_analysis_accessions to metadata_db_user;
+ALTER TABLE eva_submissions.submission_tracking_details_analysis_accessions OWNER to metadata_db_user;
+GRANT ALL ON TABLE eva_submissions.submission_tracking_details_analysis_accessions to metadata_db_user;
 
-ALTER TABLE eva_submissions.submission_details_analysis_accessions ADD CONSTRAINT fk_submission_details_analysis_accessions_submission_id FOREIGN KEY (submission_id) REFERENCES eva_submissions.submission_details(submission_id);
+ALTER TABLE eva_submissions.submission_tracking_details_analysis_accessions ADD CONSTRAINT fk_submission_tracking_details_analysis_accessions_submission_id FOREIGN KEY (submission_id) REFERENCES eva_submissions.submission_tracking_details(submission_id);
 
 
 --- table (submission_processing_status)
